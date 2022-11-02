e4e34ec0fe2b86a02a0c93a66307f438656084fc
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2022-11-02 17:07:16 +0000

    checkpoint

--- 2 neurons/phase diagrams/coupled.py
+++ 2 neurons/phase diagrams/coupled.py
@@ -6,46 +6,45 @@ import numpy as np
 
 # script constants
 NUMBER_OF_NEURONS = 2
-i = 1
-j = 0
+i = 0
+j = 1
 
 # network state
-I = [2.5, 10]
+I = [0, 0]
+s = np.array([ [0, 0.5], [0.5, 0] ])
 
 # equation constants
 g = 5
-a = [0.24, 0.63]
+a = [1, 1]
 A = 2
-H = 1.3
-B = np.array([ [0, 1.8], [1.8, 0] ])
+
+# custom constants
+Z = 10
 
 # mathematical functions
 def sigmoid(x):
-    return 1/(1+exp**(-x))
+    value = 1/(1+exp**(-x))
+    value = value - 0.5
+    value = value*2
+    return value
 
-def dudt(u, s):
-    term_1 = -u
+def dudt(neuron_id, number_of_neurons, u, I, s, g, a):
+    term_1 = -u[neuron_id]
 
-    connection_strength=sigmoid(s) # T
-    sum = connection_strength * sigmoid(u)
+    sum = 0
+    for pointer in range(number_of_neurons-1):
+        if pointer != neuron_id:
+            connection_strength=sigmoid(s[neuron_id, pointer]) # T
+            sum += connection_strength * sigmoid(u[pointer])
     term_2 = g*sum
 
-    term_3 = A * I[i]
+    term_3 = A * I[neuron_id]
 
-    derivative = 1/a[i] * (term_1 + term_2 + term_3)
+    derivative = 1/a[neuron_id] * (term_1 + term_2 + term_3)
     return derivative
 
-def dsdt(u, s):
-    term_1 = -s
-
-    term_2 = H*sigmoid(u)*sigmoid(u)
-
-    derivative = (1/B[i][j])*(term_1 + term_2)
-
-    return derivative
-
-def system(u, s):
-    return np.array([dudt(u, s), dsdt(u, s)])
+def system(u):
+    return np.array([dudt(0, NUMBER_OF_NEURONS, u, I, s, g, a), dudt(1, NUMBER_OF_NEURONS, u, I, s, g, a)])
 
 # plotting functions
 def update_plot(*args):
@@ -55,25 +54,25 @@ def update_plot(*args):
     global A
     A = A_constant_slider.val
 
-    global H
-    H = H_constant_slider.val
-
     global a
-    a[i] = a_constants_slider.val
-
-    global B
-    B[i][j] = B_constant_slider.val
+    a[i] = a0_constants_slider.val
+    a[j] = a1_constants_slider.val
 
     global I
-    I[i] = I_slider.val
+    I[i] = I0_slider.val
+    I[j] = I0_slider.val
 
-    DU, DS = system(U, S)
-    clrMap = (np.hypot(DU, DS))
+    global s
+    s[i][j] = s_slider.val
+    s[j][i] = s_slider.val
+
+    DU1, DU2 = system([U1, U2])
+    clrMap = (np.hypot(DU1, DU2))
     clrMap[ clrMap==0 ] = 1
-    DU /= clrMap
-    DS /= clrMap
+    DU1 /= clrMap
+    DU2 /= clrMap
 
-    Q.set_UVC(DU, DS)
+    Q.set_UVC(DU1, DU2)
     fig.canvas.draw()
 
 
@@ -84,33 +83,34 @@ fig.subplots_adjust(bottom=0.5, hspace=0.5)
 ax = fig.add_subplot(1,1,1)
 
 # plot quivers
-u = np.linspace(-50,50,20)
-s = np.linspace(-50,50,20)
+u1 = np.linspace(-3,3,20)
+u2 = np.linspace(-3,3,20)
 
-U, S = np.meshgrid(u, s)
-DU, DS = system(U, S)
-clrMap = (np.hypot(DU, DS))
+U1, U2 = np.meshgrid(u1, u2)
+DU1, DU2 = system([U1, U2])
+clrMap = (np.hypot(DU1, DU2))
 clrMap[ clrMap==0 ] = 1
-DU /= clrMap
-DS /= clrMap
+DU1 /= clrMap
+DU2 /= clrMap
 
-Q = ax.quiver(U, S, DU, DS, clrMap, pivot='mid')
+Q = ax.quiver(U1, U2, DU1, DU2, clrMap, pivot='mid')
 ax.set_xlabel(f'$u_{i}$')
-ax.set_ylabel("$s_{%s}$" % (str(i)+str(j)))
+ax.set_ylabel(f"$u_{j}$")
 ax.grid()
 
 g_constant_slider = Slider(plt.axes([0.25, 0.1, 0.65, 0.03]), 'g constant slider', valmin=-15, valmax=15, valinit=g, valstep=0.01)
 A_constant_slider = Slider(plt.axes([0.25, 0.15, 0.65, 0.03]), 'A constant slider', valmin=-1.5, valmax=1.5, valinit=A, valstep=0.05)
-H_constant_slider = Slider(plt.axes([0.25, 0.2, 0.65, 0.03]), 'H constant slider', valmin=-15, valmax=15, valinit=H, valstep=0.01)
-a_constants_slider = Slider(plt.axes([0.25, 0.25, 0.65, 0.03]), f'$a_{i}$ constant slider', valmin=0.1, valmax=0.5, valinit=a[i], valstep=0.01)
-B_constant_slider = Slider(plt.axes([0.25, 0.3, 0.65, 0.03]), '$B_{%s}$ slider' % (str(i)+str(j)), valmin=-1, valmax=1, valinit=B[i][j], valstep=0.05)
-I_slider = Slider(plt.axes([0.25, 0.35, 0.65, 0.03]), f'$I_{i}$ slider', valmin=-15, valmax=15, valinit=I[i], valstep=0.01)
+a0_constants_slider = Slider(plt.axes([0.25, 0.2, 0.65, 0.03]), f'$a_{i}$ constant slider', valmin=0.1, valmax=1, valinit=a[i], valstep=0.01)
+a1_constants_slider = Slider(plt.axes([0.25, 0.25, 0.65, 0.03]), f'$a_{j}$ constant slider', valmin=0.1, valmax=1, valinit=a[j], valstep=0.01)
+I0_slider = Slider(plt.axes([0.25, 0.3, 0.65, 0.03]), f'$I_{i}$ slider', valmin=-15, valmax=15, valinit=I[i], valstep=0.01)
+I0_slider = Slider(plt.axes([0.25, 0.35, 0.65, 0.03]), f'$I_{j}$ slider', valmin=-15, valmax=15, valinit=I[j], valstep=0.01)
+s_slider = Slider(plt.axes([0.25, 0.4, 0.65, 0.03]), '$s_{%s}$ slider' % (str(i)+str(j)), valmin=-10, valmax=10, valinit=s[0][1], valstep=0.1)
 
 g_constant_slider.on_changed(update_plot)
 A_constant_slider.on_changed(update_plot)
-H_constant_slider.on_changed(update_plot)
-a_constants_slider.on_changed(update_plot)
-B_constant_slider.on_changed(update_plot)
-I_slider.on_changed(update_plot)
+a0_constants_slider.on_changed(update_plot)
+I0_slider.on_changed(update_plot)
+I0_slider.on_changed(update_plot)
+s_slider.on_changed(update_plot)
 
 plt.show()
---
+++ 2 neurons/phase diagrams/coupled_learning.py
@@ -0,0 +1,116 @@
+from math import e as exp
+from matplotlib.widgets import Slider
+from scipy import integrate
+import matplotlib.pyplot as plt
+import numpy as np
+
+# script constants
+NUMBER_OF_NEURONS = 2
+i = 1
+j = 0
+
+# network state
+I = [2.5, 10]
+
+# equation constants
+g = 5
+a = [0.24, 0.63]
+A = 2
+H = 1.3
+B = np.array([ [0, 1.8], [1.8, 0] ])
+
+# mathematical functions
+def sigmoid(x):
+    return 1/(1+exp**(-x))
+
+def dudt(u, s):
+    term_1 = -u
+
+    connection_strength=sigmoid(s) # T
+    sum = connection_strength * sigmoid(u)
+    term_2 = g*sum
+
+    term_3 = A * I[i]
+
+    derivative = 1/a[i] * (term_1 + term_2 + term_3)
+    return derivative
+
+def dsdt(u, s):
+    term_1 = -s
+
+    term_2 = H*sigmoid(u)*sigmoid(u)
+
+    derivative = (1/B[i][j])*(term_1 + term_2)
+
+    return derivative
+
+def system(u, s):
+    return np.array([dudt(u, s), dsdt(u, s)])
+
+# plotting functions
+def update_plot(*args):
+    global g
+    g = g_constant_slider.val
+
+    global A
+    A = A_constant_slider.val
+
+    global H
+    H = H_constant_slider.val
+
+    global a
+    a[i] = a_constants_slider.val
+
+    global B
+    B[i][j] = B_constant_slider.val
+
+    global I
+    I[i] = I_slider.val
+
+    DU, DS = system(U, S)
+    clrMap = (np.hypot(DU, DS))
+    clrMap[ clrMap==0 ] = 1
+    DU /= clrMap
+    DS /= clrMap
+
+    Q.set_UVC(DU, DS)
+    fig.canvas.draw()
+
+
+# setup plot
+fig = plt.figure(figsize=(8,6))
+fig.tight_layout(pad=5.0)
+fig.subplots_adjust(bottom=0.5, hspace=0.5)
+ax = fig.add_subplot(1,1,1)
+
+# plot quivers
+u = np.linspace(-50,50,20)
+s = np.linspace(-50,50,20)
+
+U, S = np.meshgrid(u, s)
+DU, DS = system(U, S)
+clrMap = (np.hypot(DU, DS))
+clrMap[ clrMap==0 ] = 1
+DU /= clrMap
+DS /= clrMap
+
+Q = ax.quiver(U, S, DU, DS, clrMap, pivot='mid')
+ax.set_xlabel(f'$u_{i}$')
+ax.set_ylabel("$s_{%s}$" % (str(i)+str(j)))
+ax.grid()
+
+g_constant_slider = Slider(plt.axes([0.25, 0.1, 0.65, 0.03]), 'g constant slider', valmin=-15, valmax=15, valinit=g, valstep=0.01)
+A_constant_slider = Slider(plt.axes([0.25, 0.15, 0.65, 0.03]), 'A constant slider', valmin=-1.5, valmax=1.5, valinit=A, valstep=0.05)
+H_constant_slider = Slider(plt.axes([0.25, 0.2, 0.65, 0.03]), 'H constant slider', valmin=-15, valmax=15, valinit=H, valstep=0.01)
+a_constants_slider = Slider(plt.axes([0.25, 0.25, 0.65, 0.03]), f'$a_{i}$ constant slider', valmin=0.1, valmax=0.5, valinit=a[i], valstep=0.01)
+B_constant_slider = Slider(plt.axes([0.25, 0.3, 0.65, 0.03]), '$B_{%s}$ slider' % (str(i)+str(j)), valmin=-1, valmax=1, valinit=B[i][j], valstep=0.05)
+I_slider = Slider(plt.axes([0.25, 0.35, 0.65, 0.03]), f'$I_{i}$ slider', valmin=-15, valmax=15, valinit=I[i], valstep=0.01)
+
+g_constant_slider.on_changed(update_plot)
+A_constant_slider.on_changed(update_plot)
+H_constant_slider.on_changed(update_plot)
+a_constants_slider.on_changed(update_plot)
+B_constant_slider.on_changed(update_plot)
+I_slider.on_changed(update_plot)
+
+plt.show()
