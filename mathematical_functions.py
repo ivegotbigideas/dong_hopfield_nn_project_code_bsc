f125f745b28de9743633fd4ebd54aac4271fb2de
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2022-11-15 17:38:23 +0000

    Update fixed point location

--- mathematical_functions.py
+++ mathematical_functions.py
@@ -24,19 +24,13 @@ def dudt(u, neuron_id):
 def two_dim_system(u):
     return np.array([dudt(u, network.focal_neurons[0]), dudt(u, network.focal_neurons[1])])
 
-def find_fixed_points_of_2D_system():
+def find_fixed_points_of_2D_system_NEW():
     if network.s[network.focal_neurons[0]][network.focal_neurons[1]] >= 0:
-        hor_values = optimize.fixed_point(iter_2d_sys_fixed_point, [[-3],[0],[3]], maxiter=2000)
-        vert_values = optimize.fixed_point(iter_2d_sys_fixed_point, [[-3],[0],[3]], maxiter=2000)
+        u_inits = [[-3, -3], [0, 0], [3, 3]]
     else:
-        hor_values = optimize.fixed_point(iter_2d_sys_fixed_point, [[-3],[0],[3]], maxiter=2000)
-        vert_values = optimize.fixed_point(iter_2d_sys_fixed_point, [[3],[0],[-3]], maxiter=2000)
-    values = []
-    for index in range(len(hor_values)):
-        values.append([hor_values[index][0], vert_values[index][0]])
-    return values
-
-# This function only works in a specific case right now - a 2 neuron system with no external stimulus. It should be generalised
-def iter_2d_sys_fixed_point(u):
-    value = network.g * sigmoid(network.s[network.focal_neurons[0]][network.focal_neurons[1]]) * sigmoid(network.g * sigmoid(network.s[network.focal_neurons[1]][network.focal_neurons[0]]) * sigmoid(u))
-    return value
+        u_inits = [[-3, 3], [0, 0], [3, -3]]
+    fixed_points = []
+    for u_init in u_inits:
+        fixed_point = optimize.newton(two_dim_system, u_init, maxiter=2000)
+        fixed_points.append(fixed_point)
+    return fixed_points
