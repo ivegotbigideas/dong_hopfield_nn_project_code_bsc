e01e6ceac36997db437138710260f10756a5aaaa
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2022-11-26 11:06:19 +0000

    encapsulate

--- network_state.py
+++ network_state.py
@@ -11,20 +11,20 @@ class Network:
 
         # network state
         self.u = [0]*self.number_of_neurons
-        self.s = self.generate_s_matrix(self.number_of_neurons)
+        self.s = self._generate_s_matrix(self.number_of_neurons)
 
         # external stimulus
-        _possible_stimulus_states = self.generate_possible_stimulus_states(self.number_of_neurons)
-        self.I = _possible_stimulus_states[0]
+        self._possible_stimulus_states = self._generate_possible_stimulus_states(self.number_of_neurons)
+        self.I = self._possible_stimulus_states[0]
 
         # equation constants
         self.g = 0.3
         self.a = [1]*self.number_of_neurons
         self.A = 2
         self.H = 1
-        self.B = self.generate_B_matrix(self.number_of_neurons)
+        self.B = self._generate_B_matrix(self.number_of_neurons)
 
-    def generate_B_matrix(self, number_of_neurons):
+    def _generate_B_matrix(self, number_of_neurons):
         B = []
         for neuron_id in range(number_of_neurons):
             row = [300]*number_of_neurons
@@ -32,21 +32,21 @@ class Network:
             B.append(row)
         return B
 
-    def generate_possible_stimulus_states(self, number_of_neurons):
+    def _generate_possible_stimulus_states(self):
         possible_stimulus_states = []
         for _ in range(6): # the 6 defines that there are 6 possible states
             possible_state = []
-            for _ in range(number_of_neurons):
+            for _ in range(self.number_of_neurons):
                 neuron_stimulus = randint(0,1)
                 if neuron_stimulus == 0: neuron_stimulus = -1
                 possible_state.append(neuron_stimulus)
             possible_stimulus_states.append(possible_state)
         return possible_stimulus_states
 
-    def generate_s_matrix(self, number_of_neurons):
+    def _generate_s_matrix(self):
         s = []
-        for neuron_id in range(number_of_neurons):
-            row = [1]*number_of_neurons
+        for neuron_id in range(self. number_of_neurons):
+            row = [0]*self.number_of_neurons
             row[neuron_id] = 0
             s.append(row)
         s = np.array(s, dtype=np.float64)
