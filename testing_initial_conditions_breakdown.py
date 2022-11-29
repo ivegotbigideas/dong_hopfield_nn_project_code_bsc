8962126fb9d0fb09a928a392715540ec619ec5c3
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2022-11-29 09:05:15 +0000

    testing initial conditions breakdown

---
+++ testing_initial_conditions_breakdown.py
@@ -0,0 +1,12 @@
+from network_state import network
+from mathematical_functions import break_down_init_cons
+import sympy as sp
+
+u_values = [0]*network.number_of_neurons
+sij_values = []
+for i in range(network.number_of_neurons**2):
+    sij_values.append(i)
+init_con = u_values + sij_values
+u, s = break_down_init_cons(init_con)
+sp.pprint(s)
+
