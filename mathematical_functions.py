4cb9430626fcdae42198e00a0f5c78bb3d5c5c34
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2022-12-04 16:58:30 +0000

    stupid workaround to stupid problem

--- mathematical_functions.py
+++ mathematical_functions.py
@@ -7,7 +7,6 @@ def sigmoid(x):
     return 2/pi*np.arctan(1.4*pi*x/2)
 
 def dudt(u, s, neuron_id):
-    u = [u] # needed for 1D finding of fixed point
     term_1 = -u[neuron_id]
 
     sum = 0
@@ -21,10 +20,24 @@ def dudt(u, s, neuron_id):
     derivative = 1/network.a[neuron_id] * (term_1 + term_2 + term_3)
     return derivative
 
+def dudt_fp(u, s, neuron_id):
+    term_1 = -u
+
+    sum = 0
+    for pointer in range(network.number_of_neurons):
+        connection_strength=sigmoid(s[neuron_id, pointer]) # T
+        sum += connection_strength * sigmoid(u)
+    term_2 = network.g*sum
+
+    term_3 = network.A * network.I[neuron_id]
+
+    derivative = 1/network.a[neuron_id] * (term_1 + term_2 + term_3)
+    return derivative
+
 def find_fixed_points():
     u_inits = [-3, 0, 3]
     fixed_points = []
     for u_init in u_inits:
-        fixed_point = optimize.newton(dudt, u_init, args=(network.s, 0), maxiter=2000)
+        fixed_point = optimize.newton(dudt_fp, u_init, args=(network.s, 0), maxiter=2000)
         fixed_points.append(fixed_point)
     return fixed_points
