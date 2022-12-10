6a94050060d8250bdfb62d259c141efb1d1b462c
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2022-12-10 11:24:03 +0000

    Full 2-param bif

--- plot_2_param_bifurcation_diagram.py
+++ plot_2_param_bifurcation_diagram.py
@@ -3,10 +3,6 @@ from network_state import network
 import numpy as np
 import matplotlib.pyplot as plt
 
-# axis values
-g_values = np.linspace(0, 5, 1000)
-s_values = np.linspace = np.linspace(0, 20, 10000) # Must count from y axis outwards! i.e. must be (0, X, 10000)
-
 # setup plot
 fig = plt.figure(figsize=(8,6))
 fig.tight_layout(pad=5.0)
@@ -14,25 +10,33 @@ ax = fig.add_subplot(1,1,1)
 ax.set_xlabel("$g$")
 ax.set_ylabel("$s_{%s}$" % (str(network.focal_neurons[0]) + str(network.focal_neurons[1])))
 
-# prepare data
-data = []
-for g_value in g_values:
-    network.g = g_value
-    for s_value in s_values:
-        network.s = np.array([[0, s_value],[s_value, 0]])
-        bifurcation = test_instability_condition()
-        if bifurcation == True:
-            data.append([g_value, s_value])
-            break
+def main(g_values, s_values):
+    # prepare data
+    data = []
+    for g_value in g_values:
+        network.g = g_value
+        for s_value in s_values:
+            network.s = np.array([[0, s_value],[s_value, 0]])
+            bifurcation = test_instability_condition()
+            if bifurcation == True:
+                data.append([g_value, s_value])
+                break
 
-x_values = []
-y_values = []
-for point in data:
-    x_values.append(point[0])
-    y_values.append(point[1])
+    x_values = []
+    y_values = []
+    for point in data:
+        x_values.append(point[0])
+        y_values.append(point[1])
 
-# plot data
-ax.plot(x_values, y_values)
+    # plot data
+    ax.plot(x_values, y_values, color="b")
+
+# axis values
+g_values_arr = [np.linspace(0, 5, 1000), np.linspace(0, -5, 1000)]
+s_values_arr = [np.linspace(0, 20, 10000), np.linspace(0, -20, 10000)] # Must count from y axis outwards! i.e. must be (0, X, 10000)
+for g_values in g_values_arr:
+    for s_values in s_values_arr:
+        main(g_values, s_values)
 
 # display
 plt.show()
