9e2b7d11dda305e8c4d5e36a3c964017fbb547ff
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-01-19 21:10:00 +0000

    Remove hardcoded B

--- simulate_network.py
+++ simulate_network.py
@@ -4,10 +4,10 @@ from mathematical_functions import simulate_network
 import numpy as np
 import matplotlib.pyplot as plt
 
-init_u_values = [0]*network.number_of_neurons
-init_sij_values = [0]*(network.number_of_neurons**2)
-init_con = init_u_values + init_sij_values
-t = np.linspace(0, 8*300, 200)
+u_values = [0]*network.number_of_neurons
+sij_values = [0]*(network.number_of_neurons**2)
+init_con = u_values + sij_values
+t = np.linspace(0, 8*network.B[0][1], 1000)
 sol = odeint(simulate_network, init_con, t)
 
 for index in range(network.number_of_neurons, 2*network.number_of_neurons):
