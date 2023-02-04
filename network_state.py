8f92d5abf200ad675edfe7a33f15bf4e92746dd1
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-02-04 14:11:54 +0000

    Add support for zero stimulus

--- network_state.py
+++ network_state.py
@@ -55,33 +55,36 @@ class Network:
         return s
 
     # TODO: Make this function not gross
-    def get_I(self, t):
-        if t<0:
-            print("t<0!")
-            exit(1)
-        elif 0 <= t < 12:
-            #return self._possible_stimulus_states[0]
-            return [1, -1, 1, 1, -1, -1, 1, -1, 1, -1]
-        elif 12 <= t < 24:
-            #return self._possible_stimulus_states[1]
-            return [1, -1, 1, -1, 1, 1, -1, 1, 1, 1]
-        elif 24 <= t < 36:
-            #return self._possible_stimulus_states[2]
-            return [-1, 1, 1, 1, 1, 1, -1, 1, -1, -1]
-        elif 36 <= t < 48:
-            #return self._possible_stimulus_states[3]
-            return [-1, 1, -1, -1, -1, 1, -1, 1, 1, -1]
-        elif 48 <= t < 60:
-            #return self._possible_stimulus_states[4]
-            return [-1, 1, -1, -1, -1, 1, -1, 1, 1, -1]
-        elif 60 <= t < 72:
-            #return self._possible_stimulus_states[5]
-            return [1, 1, 1, 1, -1, 1, -1, 1, -1, -1]
-        elif t == 72:
-            #return self._possible_stimulus_states[0]
-            return [-1, -1, -1, -1, 1, -1, -1, -1, -1, -1]
-        elif t > 72:
-            return self.get_I(t-72)
+    def get_I(self, t, zero_stimulus=False):
+        if zero_stimulus == False:
+            if t<0:
+                print("t<0!")
+                exit(1)
+            elif 0 <= t < 12:
+                #return self._possible_stimulus_states[0]
+                return [1, -1, 1, 1, -1, -1, 1, -1, 1, -1]
+            elif 12 <= t < 24:
+                #return self._possible_stimulus_states[1]
+                return [1, -1, 1, -1, 1, 1, -1, 1, 1, 1]
+            elif 24 <= t < 36:
+                #return self._possible_stimulus_states[2]
+                return [-1, 1, 1, 1, 1, 1, -1, 1, -1, -1]
+            elif 36 <= t < 48:
+                #return self._possible_stimulus_states[3]
+                return [-1, 1, -1, -1, -1, 1, -1, 1, 1, -1]
+            elif 48 <= t < 60:
+                #return self._possible_stimulus_states[4]
+                return [-1, 1, -1, -1, -1, 1, -1, 1, 1, -1]
+            elif 60 <= t < 72:
+                #return self._possible_stimulus_states[5]
+                return [1, 1, 1, 1, -1, 1, -1, 1, -1, -1]
+            elif t == 72:
+                #return self._possible_stimulus_states[0]
+                return [-1, -1, -1, -1, 1, -1, -1, -1, -1, -1]
+            elif t > 72:
+                return self.get_I(t-72)
+        else:
+            return [0,0,0,0,0,0,0,0,0,0]
 
 
 def break_down_init_cons(init_cons):
