18426c55b38bdf711579f3fe55693f22272cffe2
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-02-04 17:38:11 +0000

    remove print

--- simulate_network.py
+++ simulate_network.py
@@ -8,5 +8,3 @@ sij_values = [0]*(network.number_of_neurons**2)
 init_con = u_values + sij_values
 t = np.linspace(0, 8*network.B[0][1], 1000)
 sol = odeint(calculate_network_state, init_con, t)
-
-print(sol.shape)
