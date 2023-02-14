4044f37b7075dd56ae810adde81125aa74e34189
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-02-14 09:31:41 +0000

    Plot fixed points

--- plot_fixed_points.py
+++ plot_fixed_points.py
@@ -2,6 +2,24 @@ from mathematical_functions import find_fixed_points
 from simulate_network import sol
 from network_state import network
 import numpy as np
+import matplotlib.pyplot as plt
 
 s = np.ndarray.tolist(sol[len(sol)-1, network.number_of_neurons:len(sol[0])])
-print(find_fixed_points(s))
+fixed_points = find_fixed_points(s)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+for fixed_point in fixed_points:
+    print(fixed_point)
+    fp_plot = plt.scatter(fixed_point[0],fixed_point[1], marker="x")
+
+plt.grid()
+plt.show()
