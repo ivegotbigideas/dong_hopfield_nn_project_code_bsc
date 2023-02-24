a02eb2ae87391b03529d43a19fe815e957087e43
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-02-24 13:41:25 +0000

    Static training time

--- simulate_network.py
+++ simulate_network.py
@@ -6,5 +6,5 @@ import numpy as np
 u_values = [0]*network.number_of_neurons
 sij_values = [0]*(network.number_of_neurons**2)
 init_con = u_values + sij_values
-t = np.linspace(0, 8*network.B[0][1], 1000)
+t = np.linspace(0, 8*300, 1000)
 sol = odeint(calculate_network_state, init_con, t)
