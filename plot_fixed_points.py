fdca8c2a1af9a3f4586b0f26c7cd3fd9277a8413
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-04-03 17:42:42 +0100

    basic trajectory plotting

--- plot_fixed_points.py
+++ plot_fixed_points.py
@@ -1,4 +1,5 @@
-from mathematical_functions import find_fixed_points, determine_stability
+from scipy.integrate import odeint
+from mathematical_functions import find_fixed_points, determine_stability, find_attractors_informally
 from network_state import network, refactor_s_vector
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,6 +21,7 @@ ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
+# plot fixed point
 for fixed_point in fixed_points:
     fixed_point = list(fixed_point)
 
@@ -32,5 +34,24 @@ for fixed_point in fixed_points:
         fp_plot = plt.scatter(fixed_point[0],fixed_point[1], marker="^")
     
 ax.set_aspect('equal', adjustable='box')
+
+# plot trajectory
+init_con = [1, -1, 1, 1, -1, -1, 1, -1, 1, -1]
+init_con.extend(network.s.flatten())
+t = np.linspace(0, 8*300, 500)
+traj = odeint(find_attractors_informally, init_con, t)
+
+final_traj = []
+for timestep in traj:
+    print(timestep[0:2])
+    final_traj.append(timestep[0:2])
+
+x = []
+y = []
+for point in final_traj:
+    x.append(point[0])
+    y.append(point[1])
+plt.plot(x,y)
+
 plt.grid()
 plt.show()
