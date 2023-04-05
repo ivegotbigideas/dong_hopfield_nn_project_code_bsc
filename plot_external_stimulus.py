728ed855fc1752cc8603ca95502a902bc3ca2a2e
ivegotbigideas <71772008+ivegotbigideas@users.noreply.github.com>
2023-04-05 17:55:08 +0100

    better external stim plotting

--- plot_external_stimulus.py
+++ plot_external_stimulus.py
@@ -2,6 +2,8 @@ from network_state import network
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams['text.usetex'] = True
+
 t_values = np.linspace(0, 360, 100000)
 
 I = []
@@ -13,8 +15,8 @@ for t in range(6):
     plt.axvline(x = 72*t, color = 'r', linestyle="dashed")
 
 plt.xlim(0, 288)
-plt.xlabel("$t$")
-plt.ylabel("$I_i$")
+plt.xlabel("$t$", fontsize=17)
+plt.ylabel("$I_0$", fontsize=17)
 plt.plot(t_values, I)
 plt.grid()
 plt.show()
